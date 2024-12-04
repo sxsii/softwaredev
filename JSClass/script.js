1e479c6895d6b358c2e8aa3216772a9e7f5e74c4
Your Namegit <you@example.com>
2024-12-04 19:12:42 +0100

    update?

---
+++ JSClass/script.js
@@ -0,0 +1,13 @@
+let person = "Joshua"; //primitive value
+let Person = "Joshua"; //object
+
+let me = {
+    name: "John Faith",
+    age: 35,
+    city: "Ogun"
+};
+
+//Java prefers camel casing. Nvm, only use the camel casing naming convention.
+//be very descriptive with your naming, e'g, use myFirstJob instead of job one.
+
+console.log('${me.name} is ${me.age} and lives in ${me.city}')
