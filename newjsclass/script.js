c75c45e77142f6d7d937a9eca717fcfadf4d836f
Your Namegit <you@example.com>
2025-01-15 16:24:56 +0100

    what is this

---
+++ newjsclass/script.js
@@ -0,0 +1,26 @@
+"use script";
+
+//object
+let cars = {
+    city: "Lagos",
+    country: "Nigeria",
+};
+
+//array
+let fruits = ["Apple", "Orange", "Banana"];
+fruits.push("Mango");
+console.log(fruits);
+
+//pop
+fruits.pop()
+
+//shift - removes the first element
+fruits.shift()
+
+//unshift add to the front
+fruits.unshift("Dandadan")
+
+
+for (let i = 0, i < 5, i++){
+    console.log(i);
+}
