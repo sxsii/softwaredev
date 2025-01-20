86266c01e707e5c28e71be64ebad193ebd0cd8a3
Your Namegit <you@example.com>
2025-01-20 12:47:05 +0100

    anoda wan

--- newjsclass/script.js
+++ newjsclass/script.js
@@ -33,4 +33,34 @@ while (noFruits < 5){
     noFruits++;
 }
 
+//switch
+switch(dayOfTheWeek){
+    case "Monday":
+        console.log('Today is Monday');
+        break;
+    case "Tuesday":
+        console.log('Today is Tuesday');
+        break;
+    case "Wednesday":
+        console.log('Today is Wednesday');
+        break;
+    case "Thursday":
+       console.log('Today is Thursday');
+        break;
+    case "Friday":
+        console.log('Today is Friday');
+        break;
+    case "Saturday":
+        console.log('Today is Saturday');
+        break;
+    case "Sunday":
+        console.log('Today is Sunday');
+        break;
+    default:
+        console.log('That is not a day in English.')
+    
+}
+
+
+
 //do while and functions next class
